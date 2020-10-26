f6c647300ed175f6a5bfe43a33cf76332c71d640
Hubz <33783503+Hubza@users.noreply.github.com>
2020-10-26 12:44:07 +0000

    add better logging

--- src/main/java/uk/co/hubza/telecraft/telecraft.java
+++ src/main/java/uk/co/hubza/telecraft/telecraft.java
@@ -9,10 +9,13 @@ import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Random;
 import java.util.Scanner;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
@@ -27,9 +30,32 @@ public final class telecraft extends JavaPlugin {
 	public int currenthour;
 	public BossBar bar;
 	
+	public void log(String logtext) {
+		Bukkit.broadcastMessage("[Telecraft] : " + logtext);
+	}
+	
 	@Override
     public void onEnable() {
 		getLogger().info("onEnable has been invoked! The Telecraft plugin is now active!");
+		
+		try {
+			File myObj = new File("teleplayers.txt");
+			if (myObj.createNewFile()) {
+			 	System.out.println("File created: " + myObj.getName());
+			 	log("teleplayers.txt file did not exist. Created for future use.");
+			 } else {
+				 log("teleplayers.txt already exists. That means that there was most likely a previous shutdown. Reading...");
+				 Scanner myReader = new Scanner(myObj);
+			     while (myReader.hasNextLine()) {
+			        lasthour = Integer.parseInt(myReader.nextLine());
+			        log("Read the doc. " + lasthour);
+			     }
+			     myReader.close();
+			 }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         // TODO Insert logic to be performed when the plugin is enabled
 		bar = Bukkit.createBossBar(
                 ChatColor.DARK_PURPLE + "Initializing...",
@@ -60,47 +86,47 @@ public final class telecraft extends JavaPlugin {
             	
 
             	long currentsec = calendar.getTimeInMillis();
-            	Bukkit.broadcastMessage("Currentsec is " + currentsec + ".");
+            	log("Currentsec is " + currentsec + ".");
             	long secinhour = calendar2.getTimeInMillis();
-            	Bukkit.broadcastMessage("Secinhour is " + secinhour + ".");
+            	log("Secinhour is " + secinhour + ".");
             	long nexttel = (secinhour - currentsec) / 1000;
             	
             	if((nexttel / 60 + 1) > 1) {
-            	bar.setTitle((nexttel / 60 + 1) + " Minutes until the next teleport.");
+            		bar.setTitle((nexttel / 60 + 1) + " Minutes until the next teleport.");
             	}else {
             		bar.setTitle(nexttel + " Seconds until the next teleport.");
             	}
             	
-            	float barwidth = nexttel / 3600;
+            	float barwidth = (float)nexttel / 3600;
             	
                 bar.setProgress(barwidth);
                 
-                Bukkit.broadcastMessage("Intended bar width: " + barwidth);
+                log("Intended bar width: " + barwidth);
                 
-                Bukkit.broadcastMessage("The current hour should be " + currenthour + ".");
-                Bukkit.broadcastMessage("Seconds to next teleport is " + nexttel + ".");
+                log("The current hour should be " + currenthour + ".");
+                log("Seconds to next teleport is " + nexttel + ".");
                 
-                Bukkit.broadcastMessage("Last hour is " + lasthour);
+                log("Last hour is " + lasthour);
                 bar.setVisible(true);
                 
                 if(lasthour == -1){
                     // load file where last hour was saved
-                	 Bukkit.broadcastMessage("Last hour was not set. Loading...");
+                	 log("Last hour was not set. Loading...");
                 	 
                 	 File myObj = new File("lasthour.txt");
                 	 try {
 						if (myObj.createNewFile()) {
 						 	System.out.println("File created: " + myObj.getName());
-						 	Bukkit.broadcastMessage("lasthour.txt file did not exist. Created. Will now write currenthour to the file.");
+						 	log("lasthour.txt file did not exist. Created. Will now write currenthour to the file.");
 						 	FileWriter myWriter = new FileWriter("lasthour.txt");
 						 	myWriter.write(Integer.toString(currenthour));
 						    myWriter.close();
 						 } else {
-							 Bukkit.broadcastMessage("lasthour.txt already exists. That means that there was most likely a previous shutdown. Loading.");
+							 log("lasthour.txt already exists. That means that there was most likely a previous shutdown. Loading.");
 							 Scanner myReader = new Scanner(myObj);
 						     while (myReader.hasNextLine()) {
 						        lasthour = Integer.parseInt(myReader.nextLine());
-						        Bukkit.broadcastMessage("Read the doc. " + lasthour);
+						        log("Read the doc. " + lasthour);
 						     }
 						     myReader.close();
 						 }
@@ -112,16 +138,47 @@ public final class telecraft extends JavaPlugin {
                 }
                 else{
                     if(currenthour == lasthour){
-                    	Bukkit.broadcastMessage("lasthour is the same as now. don't run anything");
+                    	log("lasthour is the same as now. don't run anything");
                     }else{
-                    	Bukkit.broadcastMessage("A new hour has began! Time to teleport.");
+                    	log("A new hour has began! Time to teleport.");
                         lasthour = currenthour;
+                        inittele();
                     }
                 }
             }
         }.runTaskTimer(this, 0, 20);
     }
     
+	@SuppressWarnings("deprecation")
+	public void inittele() {
+		for (Player player: Bukkit.getServer().getOnlinePlayers()) {
+			teleplayer(player);
+    	}
+	}
+	
+	public void teleplayer(Player player) {
+		Location spawn = Bukkit.getServer().getWorld("world").getSpawnLocation();
+		int x = 0;
+		int z = 0;
+		int maxX= x + 2500;
+		int minX= x - 2500;
+		int maxZ= z + 2500;
+		int minZ= z - 2500;
+		Random Xrand = new Random();
+		x = Xrand.nextInt(maxX - minX) + minX;
+		Random Zrand = new Random();
+		z = Zrand.nextInt(maxZ - minZ) + minZ;
+		int y = Bukkit.getServer().getWorld("world").getHighestBlockAt(x,z).getY();
+		Location loc = new Location(player.getLocation().getWorld(), x, y, z);
+		loc.getChunk().load();
+		Chunk c = loc.getChunk();
+		player.getLocation().getWorld().loadChunk(c);
+		player.getLocation().getWorld().refreshChunk(c.getX(), c.getZ());
+		loc.setY(loc.getWorld().getHighestBlockYAt(loc));
+		player.teleport(loc);
+		player.sendMessage(ChatColor.DARK_AQUA + "Teleported to: X: " + ChatColor.GOLD + x + ChatColor.DARK_AQUA + " Z: " + ChatColor.GOLD + z);
+	}
+	
     @Override
     public void onDisable() {
     	getLogger().info("onDisable has been invoked! The Telecraft plugin is now shutting down.");
@@ -139,6 +196,21 @@ public final class telecraft extends JavaPlugin {
     		sender.sendMessage("You, " + sender.getName() + ", have just ran the command /" + cmd.getName() + " : " + cmd);
     		return true;
     	} //If this has happened the function will return true. 
+    	
+    	if (cmd.getName().equalsIgnoreCase("tpplayer")) {
+    		Player other = Bukkit.getServer().getPlayer(args[0]);
+    		 
+            if(other == null)
+            {
+                sender.sendMessage(ChatColor.RED+"Error: Player "+ChatColor.YELLOW+args[0]+ChatColor.RED+" is not online");
+                return true;
+            }
+            else {
+            	teleplayer(other);
+            	return true;
+            }
+    	}
+    	
         
     	return false; 
     }
